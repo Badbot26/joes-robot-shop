582c8c5a9d126a63375c45c0dd32e0bd08db04b7
Wayne Michael <pwaynemichael@gmail.com>
2025-12-21 07:13:17 +0200

    Undo inline styles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,18 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'bot-home',
-  template: `
-    <p class='blue'>
-      home component works!
-    </p>
-  `,
-  styles: [
-    `
-      .blue {
-        color: blue;
-      }
-    `
-  ]
+  templateUrl: './home.component.html',
+  styleUrls: [ './home.component.css' ]
 })
 export class HomeComponent {
 
