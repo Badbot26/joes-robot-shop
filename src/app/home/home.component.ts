630a1f40abdcdff0b46b175b1f1313b003146823
Wayne Michael <pwaynemichael@gmail.com>
2025-12-21 07:09:02 +0200

    use inline html and css for the component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,8 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'bot-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  template: `
+    <p class='blue'>
+      home component works!
+    </p>
+  `,
+  styles: [
+    `
+      .blue {
+        color: blue;
+      }
+    `
+  ]
 })
 export class HomeComponent {
 
